01e23d2c8ee11a27e6ac966a3e3094e02bfd6548
cemno <clemensstephany@gmail.com>
2021-04-20 15:24:10 +0200

    installieren von numpy und tupel erstellt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Test')
-
+    x = (1, 2, 3, 4)
+    print(type(x))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
