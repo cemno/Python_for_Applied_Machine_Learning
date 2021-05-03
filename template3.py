0e66b249b6aeb5aae1b2376e613ea13361cf6583
cemno <clemensstephany@gmail.com>
2021-05-03 02:27:51 +0200

    all done but end of functions missing

--- template3.py
+++ template3.py
@@ -86,10 +86,12 @@ print("{1:} {0:0.02f} {2:06}".format(3.14159, 1212, 546))
 # found it overly helpful. Just put the values in .format() in the right order?
 # Can you think of a case where it might help?
 
-# faster insertion or shifting in long statements
+# faster insertion or shifting in long statements maybe?
 
 # Overall you can pick the version you find easier.
-
+'''
+Why not include repr() as an function, that always returns the object as printable.
+'''
 
 """
   ######## 2. For and While loops
@@ -142,16 +144,26 @@ while x > y:
 # that uses a new function "range". Range takes as input at least an upper limt, and
 # returns a range of values. Let's play with that now, x = range( lower value, upper value )
 # do this now for 0 to 10 then print it, what do you notice about the upper value?
+print("\n---- for loops ----")
+x = range(0,10)
+for i in x: # 10 not included
+    print(i)
 
 
-# what happens if we envoke the list class
+# what happens if we invoke the list class
+print("\n---- for loop with list ----")
+x = list(x)
+print("type of x: " + repr(type(x)))
+for i in x:
+    print(i)
 
 
 # however we don't need to do this for for loops. "range" has an iterator member that
 # allows you to move over the values in the class 0->10 in this case.
 # to simplify this, if we are starting at zero we can just do:
 # x = range( upper limit ) try it now
-
+for i in range(10):
+    print(i)
 
 # how does this help us with a for loop?
 # well like I said we need something to iterate over, and a "range" is a perfect
@@ -165,27 +177,37 @@ while x > y:
 # let's create a list of values [0,2,4,6,8,10,12,14,16,18,20]
 # now let's create a for loop that iterates around this list using an iterator
 # that depends on the "len" function.
-# 'len' does exactly what you expect, get's the lenght of something.
+# 'len' does exactly what you expect, gets the length of something.
 # so len returns an integer, we can now use this in "range"
 # for i in range( len( list ) )
 # try this now with a print function
 
+list1 = [0,2,4,6,8,10,12,14,16,18,20]
+print("\nprint range of length of this list: \n" + repr(list1))
+for item in range(len(list1)):
+    print(item)
 
 # essentially this is the same as:
 # for i in x:
 # but this gives us the addition of an iterator, this can come in handy at different
 # times.
 # Try doing this at home:
-
+print("\nprint this list: \n" + repr(list1))
+for i in list1:
+    print(i)
 
 # Finally, for the basics let's consider dictionaries.
-# d = {'str':'dog', 'pi':3.14159, 'int':1212}
+d = {'str':'dog', 'pi':3.14159, 'int':1212}
 # how do you think you would iterate through this? HINT remember keys()?
+print("\nprint dict: ")
+for item in d: #or d.keys(): print(d[item])
+    print(repr(item) + ": " +  repr(d[item]))
 
-
-# we can also do this another way by envoking .items() this returns the key and
+# we can also do this another way by invoking .items() this returns the key and
 # the value.
 
+for item in d.items():
+    print(item)
 
 # again different pieces of code call for different approaches. Just make sure
 # you understand that there are usually multiple ways to do the same thing.
@@ -200,22 +222,43 @@ while x > y:
 # def <function name>(input0, input1, ..., inputN):
 # let's create a function called "rectangle_area" that calculates the area of
 # a rectangle based on two inputs h and w.
+print("\n---- Functions ----")
+def rectangle_area(a, b): # how do i define input type?
+    return a*b
+print("rectangle_area(3,4) = " + repr(rectangle_area(3,4)))
 
-
-# Along with having mutiple inputs you can also use multiple outputs.
+# Along with having multiple inputs you can also use multiple outputs.
 # return out0, out1, ..., outM
 # let's try this with the properties of a circle based on an input radius.
 # first we need to define pi, it's up to you how many decimals you define it to.
+'''
+def properties_circle(self):
+    area = self.radius * (numpy.pi ** 2)
+    circumference = 2 * numpy.pi * self.radius
+    diameter = 2 * self.radius
+    return area, circumference, diameter
+'''
 
+class circle:
+    '''
+    numpy needed
+    '''
+    def __init__(self, radius):
+        self.radius = radius
+        self.area = radius * (numpy.pi ** 2)
+        self.circumference = 2 * numpy.pi * radius
+        self.diameter = 2 * radius
+    def print_circle(self):
+        print("My circle: ")
+        print("area: " + repr(self.area) + "\ncircumference: " + repr(self.circumference) + "\ndiameter: " + repr(self.diameter))
+
+my_circle = circle(5.5)
+my_circle.print_circle()
 
 # or alternatively we can use the inbuilt pi from the numpy library.
 # Do you remember how to import numpy, try it now
-
-
 # the internal numpy pi can be called simply as np.pi
-
-
-# now let's create a function that takes as input, r(adius) and pi
+# now let's create a function that takes as input, radius) and pi
 # and outputs the diameter, the circumference, and the area.
 # and then output each of them.
 
@@ -246,7 +289,9 @@ while x > y:
 # = circle_properties(
 # now let's output this data
 # return
-
+'''
+I don't understand what i am supposed to do.
+'''
 
 # display the results.
 
@@ -278,25 +323,31 @@ while x > y:
 # Basically, we open a file, we write information to that file, and IMPORTANT we
 # close the file. There is an easy way to do this, but for now we will do open and
 # close as different functions to make it simple to remember.
-# To open a file we need two pieces of information, the file name, and the openning
+# To open a file we need two pieces of information, the file name, and the opening
 # properties like read (r), write (w), or append (a).
-# fid = open( 'file.txt', 'w' ) # I'll set this to write as I do want to competely
+# fid = open( 'file.txt', 'w' ) # I'll set this to write as I do want to completely
 # overwrite what is in that file.
 # don't forget fid.close()! I always write both pieces of code straight away
 # when I do this. Let's do it
 
+with open("file.txt", 'w') as file:
+    #automaticly closes because of with, why not teach this on?
+    pass
 
 # Look in the file you just created. It should be in your working directory
 # Now how do we add stuff to a file?
 # There are two options:
 # fid.write( <string to write> ) does not automatically put a new line escape in
-# print( <string to write>, file=fid  ) Remeber this does put a new line escape automatically
+# print( <string to write>, file=fid  ) Remember this does put a new line escape automatically
 # Now create a new file (don't forget to close it!).
 # create a string with the end line character '\n' at the end.
 # inside this string insert a integer and float.
 # use both the write and print to insert it into the file.
 # Go.
 
+with open("file.txt", 'w') as file:
+    file.write("Write")
+    print("\nPrint" + " float: " + repr(float1) + " integer:" + repr(int1) + "\n", file = file)
 
 # look in the file you just created.
 # what happens if you switch the end of line characters?
@@ -309,6 +360,8 @@ while x > y:
 # remember you will use the 'a' instead of 'w'. Once you have done this, look into
 # that file and see what is there?
 
+with open("file.txt", 'a') as file:
+    file.write("Adding something\n")
 
 # now what if we want to read in that information? Let's use the first file
 # we created that has something in it, and that we appended too.
@@ -317,12 +370,22 @@ while x > y:
 # text files. fid.readlines() reads in all the lines of in a file. Try to work out,
 # using a for loop, how to display all the information to the screen.
 
+with open("file.txt", 'r') as file:
+    print()
+    for line in file.readlines(): # "line in file" works directly
+        line = line[:-1]
+        print(line)
 
 # now a second way to do the same thing. Using readline. This way iteratively
 # reads each line in the text file, compared to readlines which reads them all.
 # now because we don't know the end condition we have to use a while loop.
 # see if you can work it out.
-
+with open("file.txt", 'r') as file:
+    print()
+    line = file.readline()
+    while line:
+        print(line[:-1])
+        line = file.readline()
 
 # can anyone think of a problem with this method? what if l is empty deliberately?
 # try it, manually put an empty line in the file.
@@ -334,9 +397,12 @@ while x > y:
 # or even classes? In this case we would use the pickle library. It's a great tool
 # for saving and loading information. The first thing we need to do is import pickle
 
+import pickle
 
 # let's create a list and a dictionary. You decide what goes in them.
 
+list1 =  [1,2,3]
+dict1 = {"one": 1, "two": 2, "three": 3}
 
 # now we will create a pickle and save them independently.
 # We will do this in exactly the same way we would a text file from above.
@@ -347,20 +413,35 @@ while x > y:
 # the file should end in .pkl or something similar for standardisation.
 # Create a file and pickle.dump the list, then create another one and pickle.dump
 # the dictionary. Don't forget to close them!
-
+print("\nload and save each line separately")
+with open("file.txt", 'wb') as file:
+    pickle.dump(list1, file = file)
+    pickle.dump(dict1, file = file)
 
 # now how do we read this information back in?
 # in this case we will use pickle.load( fid ). So obviously we need to open a file
 # id here. Remember we need the binary flag as well. Try to load and print the information.
 
+with open("file.txt", 'rb') as file:
+    list1 = pickle.load(file)
+    dict1 = pickle.load(file)
+print(list1)
+print(dict1)
 
 # but what if we want to save both the list and the dictionary?
 # well you have multiple options, you can store them as a list, tuple, or dictionary.
 # let's do it as a tuple. Save both the list and dictionary in one pickle.
-
+print("\nsave and load as tuple")
+with open("file.txt", 'wb') as file:
+    dump = (list1, dict1)
+    pickle.dump(dump, file = file)
 
 # now let's try and load this?
 
+with open("file.txt", 'rb') as file:
+    dump = pickle.load(file)
 
+for item in dump:
+    print(item)
 # Obviously the key here is that you need to know what is in your pickle, this only works
 # for tuples or lists. Try it in your own time.
