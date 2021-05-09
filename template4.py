e858a80400d2a51d587d4374a4896916ec12849d
cemno <clemensstephany@gmail.com>
2021-05-09 17:04:36 +0200

    added template4.py

---
+++ template4.py
@@ -0,0 +1,253 @@
+"""
+  week 3 topics
+  1. classes
+  2. data visualisation
+  3. image manipulation
+"""
+
+"""
+  ####### Import area
+"""
+import numpy as np # we always seem to use this so I will import it for you.
+
+
+"""
+  ####### Preamble
+"""
+# here are the flags we will use to run this.
+# In future you will create these. See below how they are used. This is just to
+# benefit us in reading the outputs. Previously we have had all the exercises
+# printed to screen. Now we can choose which ones we print.
+runclass = False
+runvisual = False
+runimage = False
+
+"""
+  ####### 1. Classes
+"""
+# arew we running the class information?
+if runclass:
+  # Classes are a great way to group common methods (functions) and members (variables) together in one space.
+  # In last weeks practical we created a function that calculated the diameter, circumference,
+  # and the area. Now we will do exactly the same thing in a class.
+  # The inbuilt function __init__ is something that we will use througout our classes.
+  # When you initialise the class you can insert information that get stored.
+  # First let's create the class called circle_props
+
+
+    # The next step is to assign any shared variables.
+    # These are static variables that are assigned to all circle_props classes
+    # pi = np.pi # this creates a member self.pi equal to np.pi
+    # We could do this here but I prefer to pass them in a different way.
+    # Instead we will use an inbuilt class method __init__( self, )
+    # __init__ does exactly what you would expect, creates the ability to initialise a class
+    # based on input variables.
+    # NOTE: For all methods of a class the first variable needs to be self, this ensures
+    # the "function" is assigned to the class, giving it access to all class members and methods.
+    # Let's create the __init__ method with a radius and pi input.
+
+
+      # to assign an input variable to the class we need to use self.<variable name> = inputn
+      # try to assign radius to self.r and pi to self.pi
+
+
+    # so now self.r and self.pi are members in the class that we can use in any of our functions.
+    # Okay, so let's try it. Create a method that calculates the diameter based on self.r
+    # Don't forget self as the first varaible
+
+
+    # Now create a method for the other two, you'll need self.pi
+
+
+    # aggregation method
+
+
+    # the update method
+
+
+  # okay let's calculate these statistics?
+  # first we create the class, remember the parameters that were in __init__.
+
+
+  # output the statistics individually
+
+
+  # okay so we can now output the statistics individually, what about if we want to do it
+  # all in one go.
+  # Go back to your class and insert a method calc_all( self, ) that uses the other three
+  # functions to output in a single line. In this case, in exactly the same way we call
+  # members we can call methods. self.<methodname>()
+  # Then use that new method and output the results.
+
+
+  # Now, what if we want to update the radius? In this case we'd have to create a whole new
+  # class. Instead, let's create an update() function. That takes a new radius as input
+  # and changes self.r... The run it all again and update then output the results
+
+
+  # This is the basics of how to build up a class. Over the course of these practicals
+  # I will slowly introduce more information to you that will improve the way you build them.
+
+"""
+  ####### 2. data visualisation
+"""
+# Let's do some plotting with matplotlib
+# Plotting is a great tool for visualising data. I have set up five exercises here
+# but we will only do three of them. I really encourage you to try the others on your
+# own. The solutions will be provided for you.
+# The first thing you need to do is add import matplotlib.pyplot as plt in your import area.
+# Are we running the visual? Do you need to change the flags?
+#if runvisual: #UNCOMMENT THIS LINE!!!
+  # Okay so let's do some basic plots
+  # So what we need is data on an x axis and data on a y axis: x and f(x).
+  # In this case x will be an equally spaced vector (numpy). We will use the np.linspace( start, finsish, number of steps )
+  # function to do this. Let's try and create data starting at 0, ending at 6 pi's, with 100 steps.
+
+
+  # great, now what about f(x). Well numpy also has functions like sin and cos, let's do np.sin( x ) for our f(x)
+
+
+  # now the interesting stuff. Let's create a figure using plt.figure()
+  # input the x label using plt.xlabel( <insert name> ) and f(x) label with plt.ylabel( <insert name> )
+
+
+  # finally let's plot the data with the basic plt.plot() function... Can you work it out?
+
+
+  # and then we need to show it
+
+
+  # What about if we want to plot multiple things on the same plot?
+  # Can you work it out? Try it in your own time.
+  # We already have x and fx which we will now change to sinx
+
+
+
+  # Now let's create a plot using subplots. So let's say 4 plots in a 2*2 matrix.
+  # We will again use the linspace for x that we created earlier.
+  # create sin(x) cos(x) sin(x**2) sin(x)/4
+
+
+  # Now we will use the plt.subplots( rows, cols ) where we have a matrix of plots.
+  # but in this case we return a fig and an axis from plt.subplots(). We have 4 plots so we'll do it as a 2*2 matrix.
+
+
+  # Creating the title on the 'fig' using fig.suptitle not very intuitive but basically a super title.
+
+
+  # Now let's plot on the first axis, [0,0] in the same way that we plt.plot but in this case ax[row,col].plot()
+
+
+  # set the axis titles in this case we need to use ax[row,col].set( xlabel=<string>, ylabel=<string> )
+
+
+  # now let's set the title... it's similar again but we use .set_title( <string> )
+
+
+  # What about the other 3 plots... but after the x and y in plot(x,y) we'll change the colours.
+  # Example plot( x, y, 'r' ) will plot red
+  # plot( x, y, '--r' ) will plot dashed lines in red
+  # plot( x, y, ':g' ) will plot dotted green lines. You could also use b for blue...
+
+
+  # now let's show the plot
+
+
+  # now let's run the script again but increase the number of steps in linspace. Say 2000, and what do you see?
+
+  # Now we are going to do some scatter plotting, another very handy plotting tool for data visualisation.
+  # But first we need to create some data, and in this case we will create normally distributed data along the x and y axis.
+  # to do this we need a mean location as a numpy array: np.array( (mu_x, mu_y) )
+  # and a sigma (covariance): np.array( [[x0, x1][y0, y1]] ). For more information on mean and covariance for the normal distribution
+  # please see: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
+  # let's create the numpy arrays for the mean and covariances, dist0 = mu[3,2] cov[[1, 0.5][0.5,1]]
+  #                                                             dist1 = mu[-1,1] cov[[2,0][0,2]]
+
+
+  # Okay now to create some normally randomly distributed points.
+  # For this we need np.random.multivariate_normal( mean, cov, number of points )
+  # Try to do that now based on what we just created, remember we need two distributions. And we'll create 100 points.
+
+
+  # okay now we have two distributions, now we just need to plot them.
+  # let's look at the data itself using the .shape function, plot the shape of both distributions.
+
+
+  # In this case we manually create a figure, plt.figure()
+
+
+  # Then we need to do a scatter plot in the same way we did plot in the first example.
+  # Where scatter( <x data of distribution N>. <y data of the distribution N>, c=<colour string like 'red'>, label=<legend name> )
+
+
+  # now label the axis and give it a title plt.xlabel, ylabel, title
+
+
+  # now show the figure
+
+
+  # but this time we will also use plt.savefig( <savename.pdf> ) to save a version of the figure.
+
+
+  # view the image, it should be in your working directory.
+
+
+  # Finally in your own time try to plot a histogram...
+  # plotting a histogram and the distributions
+  # this is just a 1d plot so only 1 mean and deviation (sigma) are required
+  # In the previous example we did multivariate, now it's single...
+
+
+"""
+  ####### 3. image manipulation
+"""
+if runimage:
+  # Now we will play with some images. As part of this weeks data you should have also downloaded two images.
+  # one of these images is an RGB image, the other is a black and white image.
+  # In this case we call the RGB image the input and the other a mask or label image.
+  # These are two very important parts of machine learning, the RGB image is fed to a model, and we try to get it to
+  # predict the mask...
+  # But let's just play with the images for now. First let's import imsave and imload from skimage.io.
+  # In the import section you will do: from skimage.io import imsave, imread
+  # This does exactly what is said in the line, now we can use imsave or imread with out needing to preface it with
+  # skimage.io like we do with np.array( [] )...
+  # So let's use imread to read in week03_rgb.png and week03_msk.png: imread( <string of file.png> )
+
+
+  # okay so let's look at the image shapes, this gets loaded in as a numpy array.
+
+
+  # now let's look at the statistics of the RGB image.
+  # We will use min() max() mean() and std()
+
+
+  # let's discuss these values in the class.
+  # what about if we define an axis to calculate the mean? So we had 1280*720*3, h*w*c
+  # what do you think will happen if we calculate the min( axis=2 )? What is the second axis here?
+
+
+  # now we will use two colour space conversion. rgb to grey scale and rgb 2 Lab.
+  # Does anyone know the problem with the RGB colour space?
+  # colour spaces: https://scikit-image.org/docs/dev/api/skimage.color.html
+  # First let's do a conversion to grey scale using rgb2gray, but first we need to import it!
+  # We can import skimage.color but every time we wanted to use it we need to go import skimage.color.something.
+  # This can become cumbersome, let's do something different to simplify it:
+  # import skimage.color as skcol
+  # So now we can do skcol.rgb2gray( rgb image )
+
+
+  # and now save it using imsave( <savename.png>, image )
+
+
+  # now let's do it with a slightly more interesting colour space.
+
+
+  # The cool thing about the lab space is that it isn't additive! It uses the channels in a different way to RGB.
+  # Channel 0 is the luminance, and 1,2 are the colours (chromanance).
+  # So we can illuminate the image using channel 0 by adding or subtracting values then converting them back to
+  # RGB. This is significantly more difficult in the RGB space.
+  # Let's add 20 to the 0'th channel of the lab image then convert it back to RGB and save it.
+
+
+  # now visually compare the two images.
+  # This is the end of this weeks practical.
