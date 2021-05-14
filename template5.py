b7004ba0e83fe627b07e4a6b0caa43f7d62a5c4a
cemno <clemensstephany@gmail.com>
2021-05-14 19:51:10 +0200

    almost done

--- template5.py
+++ template5.py
@@ -11,15 +11,16 @@
 import pickle
 import numpy as np
 import pandas as pd
+import random
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 """
   ####### Preamble
 """
-runex1 = True
+runex1 = False
 runex2 = False
-runex3 = False
+runex3 = True
 
 
 # Helper function for you to use!
@@ -124,16 +125,24 @@ if runex2:
     # and investigate the returning dictionary.
     housing = extract_data("data/week05/Practical5/week04_housing.csv")
     print(housing)
+    print(housing[0]["latitude"])
+    # What do you think the dependent value is here?
 
-    # What do you think the depenedent value is here?
+    # i would guess the median_house_value is dependent on the median_income of an individual.
 
-    # now let's plot the depenedent versus the others to see if we can spot a good
+    # now let's plot the dependent versus the others to see if we can spot a good
+    plt.figure()
+    plt.xlabel("Median house value")
+    plt.ylabel("Median income")
+    plt.title("Property dependent on income")
+    plt.scatter(housing[0]["median_house_value"], housing[0]["median_income"], c="red", alpha=0.2)
+    plt.savefig("data/week05/Practical5/income_house-value.png")
     # independent value HINT use the scatter plot function, don't forget to import.
 
     # so from that visual inspection, keeping in mind we want a linear relationship,
     # which would you select?
     # Run it again if you need to. Once you have selected one, comment that for loop out.
-    # Now save this variable versus the depenedent variable using savefig...
+    # Now save this variable versus the dependent variable using savefig...
 
     # So that's how we might go about selecting variables using a visual inspection.
     # This isn't always possible especially when you consider quadratic relationships or even noisey data.
@@ -143,69 +152,88 @@ if runex2:
 """
   ###### 3. Linear regression
 """
-# if runex3:
-# So in the previous exercise we selected a dependent and independent variable.
-# Let's see if we can train a linear regression model that fits this information together.
-# And then finally we will use the metrics from exercise 1 to evaluate the performance
-# our our systems.
-# 1. Let's load the data again and get the median income as x and the median house value as y
+if runex3:
+    # So in the previous exercise we selected a dependent and independent variable.
+    # Let's see if we can train a linear regression model that fits this information together.
+    # And then finally we will use the metrics from exercise 1 to evaluate the performance
+    # our our systems.
+    # 1. Let's load the data again and get the median income as x and the median house value as y
+    housing = extract_data("data/week05/Practical5/week04_housing.csv")
+    # Now we need to split these into a training and testing set.
+    # There are a number of ways to do this, I will step you through one variation, and
+    # not a very efficient solution.
+    # 1. import random in your import area.
+    # 2. Decide on a training/testing split. Let's go with 50% for now (0.5) of the number of rows
 
+    # 3. Create a "traininglist" of randomly assigned indexes that will select data from
+    # both x and y. To do this we will use random.sample( A, B ), where A is the full list of
+    # numbers we can select from: range(0, 20640) in this case. And B is the number of
+    # values you are going to select out of A. Try it now
+    data_length = housing[1]
+    trainList = random.sample(range(0, data_length), int(data_length / 2))
+    # print(len(trainList))
 
-# Now we need to split these into a training and testing set.
-# There are a number of ways to do this, I will step you through one variation, and
-# not a very efficient solution.
-# 1. import random in your import area.
-# 2. Decide on a training/testing split. Let's go with 50% for now (0.5) of the number of rows
+    # Now we need to create the evaluation list.
+    # Basically you will just go through range( numrows ) and if an integer is not in
+    # trainlist you will put it in evallist. Hopefully you see that this is not very
+    # efficient..
+    evalList = []
+    for i in range(data_length):
+        if i not in trainList:
+            evalList.append(i)
 
+    #
+    # print(len(evalList))
 
-# 3. Create a "traininglist" of randomly assigned indexes that will select data from
-# both x and y. To do this we will use random.sample( A, B ), where A is the full list of
-# numbers we can select from: range(0, 20640) in this case. And B is the number of
-# values you are going to select out of A. Try it now
+    # So now we have a list of index's that correspond to the training and evaluation
+    # samples. Let's use these indexes on x and y to create the subsets.
 
+    x_train = [housing[0]["median_income"][i] for i in trainList]
+    x_eval = [housing[0]["median_income"][i] for i in evalList]
 
-# Now we need to create the evaluation list.
-# Basically you will just go through range( numrows ) and if an integer is not in
-# trainlist you will put it in evallist. Hopefully you see that this is not very
-# efficient..
+    y_train = [housing[0]["median_house_value"][i] for i in trainList]
+    y_eval = [housing[0]["median_house_value"][i] for i in evalList]
 
+    # Now we have the data to train a model and the data to evaluate how good our model is.
+    # Let's plot these two sub sets individually.
+    plt.figure()
+    plt.subplot(211)
+    plt.scatter(y_train, x_train, c="red", alpha=0.2)
+    plt.subplot(212)
+    plt.scatter(y_eval, x_eval, c="green", alpha=0.2)
+    # plt.show()
+    plt.savefig("data/week05/Practical5/train_vs_eval.png")
 
-# So now we have a list of indexs that corrospond to the training and evaluation
-# samples. Let's use these indexes on x and y to create the subsets.
+    # Okay now we'll do the linear regression using sklearn. First you need to import
+    # the linear regression function... from sklearn.linear_model import LinearRegression
+    # First instantiate the class (there are some hints in the pdf).
+    linreg = LinearRegression()
+    # Try and fit using your current data. What error comes up?
+#    linreg.fit(x_train, y_train)
+    # So we have to fix the data so that it's the correct shape. The notification actually
+    # tells you how to go about this. There are a number of ways to go about this, but try
+    # the recommended version, however make the size (-1, 1) not (1, -1) like suggested.
+    x_train = np.asarray(x_train)
+    x_eval = np.asarray(x_eval)
+    y_train = np.asarray(y_train)
+    y_eval = np.asarray(y_eval)
+    x_train = x_train.reshape(-1, 1)
+    x_eval = x_eval.reshape(-1, 1)
+    # you could have also done the following
 
+    # don't forget to do it for the text set too.
 
-# Now we have the data to train a model and the data to evaluate how good our model is.
-# Let's plot these two sub sets individually.
-
-
-# Okay now we'll do the linear regression using sklearn. First you need to import
-# the linear regression function... from sklearn.linear_model import LinearRegression
-# First instantiate the class (there are some hints in the pdf).
-
-
-# Try and fit using your current data. What error comes up?
-
-
-# So we have to fix the data so that it's the correct shape. The notification actually
-# tells you how to go about this. There are a number of ways to go about this, but try
-# the recommended version, however make the size (-1, 1) not (1, -1) like suggested.
-
-
-# you could have also done the following
-
-
-# don't forget to do it for the text set too.
-
-
-# now let's fit the model with the appropriately shapped data.
-
-
-# So inside the class we now have a trained model. Let's predict the test set based on
-# this model!
-
-
-# now let's plot ypred as a line and y_test as a scatter versus x_test
-
-
-# now let's use our metrics to see how accurately we were able to predict things?
-# Remember we had these set up as (1,N) matrix...
+    # now let's fit the model with the appropriately shaped data.
+    linreg.fit(x_train, y_train)
+    print("R²: " + repr(round(linreg.score(x_train, y_train),2)))
+    # So inside the class we now have a trained model. Let's predict the test set based on
+    # this model!
+    y_pred = linreg.predict(x_eval)
+    # now let's plot ypred as a line and y_test as a scatter versus x_test
+    plt.figure()
+    plt.scatter(x_eval, y_eval, c = "red", alpha = 0.2)
+    plt.plot(x_eval, y_pred, c = "black")
+    plt.show()
+    # now let's use our metrics to see how accurately we were able to predict things?
+    # Remember we had these set up as (1,N) matrix...
+    pass
