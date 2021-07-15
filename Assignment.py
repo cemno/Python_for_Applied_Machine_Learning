66dca0c7333d26bfdeb694cf8e5a491b0e80e535
cemno <clemensstephany@gmail.com>
2021-07-15 18:26:06 +0200

    kmeans for task 1

--- Assignment.py
+++ Assignment.py
@@ -13,6 +13,8 @@ from sklearn.linear_model import LinearRegression
 
 
 ex01 = True
+pie_chart = True
+plot_cluster = False
 ex02 = True
 ex03 = True
 ex04 = True
@@ -215,19 +217,61 @@ class KMeans_self():
     # return the assignments.
     return np.argmin(dist, axis=1)
 
-# Arranging data
-train_data = np.vstack([picture_background["train"], picture_red["train"]])
-print(train_data.shape)
-# Creating k_means class
-kmeans = KMeans(2)
+# Arranging data # Addition: no need to stack, doing a kmeans with one cluster is good enough, sorting data to the correct labels later on
+#train_data = np.vstack([picture_background["train"], picture_red["train"]])
+#print(train_data.shape)
+# Creating k_means classes
+train_data_combined = np.vstack([picture_background["train"], picture_red["train"], picture_yellow["train"]])
+validation_data_combined = np.vstack([picture_background["validation"], picture_red["validation"], picture_yellow["validation"]])
 
-# Fitting data
-kmeans.fit(train_data)
-Y = kmeans.predict(train_data)
-print(Y[Y == 0].sum(), Y[Y == 1].sum())
-labels = kmeans.labels_
-print(labels)
-labels=list(labels)
+kmeans_background = KMeans(1)
+kmeans_red = KMeans(1)
+kmeans_yellow = KMeans(1)
+random.seed(1234)
+kmeans_all = KMeans(6, random_state=0) # Whats the optimal cluster number?! 6 Clusters first time yellow/orange gets separated from red
+
+
+## Fitting data
+kmeans_background.fit(picture_background["train"])
+print(kmeans_background.cluster_centers_)
+kmeans_red.fit(picture_red["train"])
+print(kmeans_red.cluster_centers_)
+#kmeans_yellow.fit(picture_yellow["train"])
+#print(kmeans_yellow.cluster_centers_)
+
+# Fit data to satisfy _n_threads in kmeans object, which handles errors if no fitting was done
+kmeans_all.fit(train_data_combined)
+print(kmeans_all.cluster_centers_)
+# Change cluster_centroids to the ones calculated by each KMeans object for each Set
+#kmeans_all.cluster_centers_ = np.vstack([kmeans_background.cluster_centers_, kmeans_red.cluster_centers_])
+
+
+## Classifiy data based on its distance to the clusters using predict
+#Y_background = kmeans_background.predict(picture_background["validation"])
+#Y_red = kmeans_red.predict(picture_background["validation"])
+
+Y_all = kmeans_all.predict(validation_data_combined)
+print(Y_all)
+
+print(np.where(Y_all == 0)[0].size, np.where(Y_all == 1)[0].size)
+
+if pie_chart:
+  labels_all = kmeans_all.labels_
+  print(labels_all)
+  labels=list(labels_all)
+  centroid=kmeans_all.cluster_centers_
+  print(centroid)
+
+  percent=[]
+  for i in range(len(centroid)):
+    j=labels.count(i)
+    j=j/(len(labels))
+    percent.append(j)
+  print(percent)
+
+  plt.pie(percent,colors=np.array(centroid/255),labels=np.arange(len(centroid)))
+  plt.show()
+
 # now use our data on this
 # Let's use this class to cluster some data (D0 from exercise 1) with 4 clusters to start with
 # and an imax of zero (we only randomly assign centers).
@@ -239,15 +283,32 @@ labels=list(labels)
 # let's plot what this looks like
 # but first we want to know the  unique values in D0a so we aren't constantly having
 # to change the label values, you'll need np.unique
-Yu = np.unique(Y)
-print(Yu.shape)
-# now scatter plt based on the predictions
-plt.figure()
-for i in Yu:
-  plt.scatter(train_data[Y == i, 0], train_data[Y == i, 1], label='{}'.format(i))
-plt.legend()
-# plt.show()
-plt.close()
+if plot_cluster:
+  Yu = np.unique(Y_all)
+  print(Yu.shape)
+  # now scatter plt based on the predictions
+  plt.figure()
+  for i in Yu:
+    plt.scatter(validation_data_combined[Y_all == i, 0], validation_data_combined[Y_all == i, 1], label='{}'.format(i))
+  plt.legend()
+  plt.show()
+  plt.close()
+
+#1280,720,3 -> 921600,3
+# Read in image and transfrom to mask
+img_input = imread("data/week04/week03_rgb.png")
+height = img_input[0,:,0].size
+width = img_input[:,0,0].size
+colors = img_input[0,0,:].size
+
+img_transformed = img_input.reshape(-1,colors)
+print(img_transformed.shape)
+index_classes = kmeans_all.predict(img_transformed)
+for cluster in range(np.min(index_classes), np.max(index_classes)+1):
+  # zuweisen der einzelnen Klassen und umschreiben eines zero arrays (alles was nicht rot oder geld ist ist ja immer klasse 0)
+  pass
+#img_mask_transformed = np.where(index_classes == 0, img_transformed[], img_transformed[:,[0,0,0]])
+#print(img_mask_transformed.shape)
 # Now what's wrong with what we did? We fit and predicted on the same  data.
 # Go back and Fit with D0 and predict with D1... How does that look?
 # Now we need to evaluate this,  for that we will use
